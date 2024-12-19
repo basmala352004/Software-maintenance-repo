ae3131fd783af9b277cbb87769c92232ecdd2343
Fatimah3844 <148279022+Fatimah3844@users.noreply.github.com>
2024-12-19 21:24:25 +0200

    Update CourseRepository.java

--- LMS/src/main/java/com/example/LMS/repositories/CourseRepository.java
+++ LMS/src/main/java/com/example/LMS/repositories/CourseRepository.java
@@ -2,7 +2,9 @@ package com.example.LMS.repositories;
 
 import com.example.LMS.models.CourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<CourseModel, Long> {
     CourseModel findByCourseId(String courseId);
+     Optional<CourseModel> findByTitle(String title);
 }
